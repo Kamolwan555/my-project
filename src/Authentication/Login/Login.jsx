fd6b8b4b136e450388db0b5550a3e12b5ff2c5cb
KamolwanSangnoo <pppor816@gmail.com>
2024-12-27 13:47:02 +0700

    Add image to login page.

--- src/Authentication/Login/Login.jsx
+++ src/Authentication/Login/Login.jsx
@@ -1,5 +1,6 @@
 import { Button, Checkbox, Form, Grid, Input, theme, Typography } from "antd";
 import { LockOutlined, MailOutlined } from "@ant-design/icons";
+import picture from "../../components/extended/background/picture.png";
 import { useNavigate } from "react-router-dom";
 
 const { useToken } = theme;
@@ -41,103 +42,113 @@ export default function Login() {
   };
 
   const styles = {
+    section: {
+      display: "flex",
+      minHeight: "100vh",
+    },
     container: {
       margin: "0 auto",
       padding: screens.md
         ? `${token.paddingXL}px`
         : `${token.sizeXXL}px ${token.padding}px`,
       width: "380px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flex: 1,
+    },
+    header: {
+      marginBottom: token.marginXL,
+      textAlign: "center",
+    },
+    title: {
+      fontSize: screens.md ? token.fontSizeHeading2 : token.fontSizeHeading3,
+    },
+    text: {
+      color: token.colorTextSecondary,
     },
     footer: {
       marginTop: token.marginLG,
       textAlign: "center",
       width: "100%",
     },
-    forgotPassword: {
-      float: "right",
-    },
-    header: {
-      marginBottom: token.marginXL,
-      textAlign: "center",
-    },
-    section: {
-      alignItems: "center",
-      backgroundColor: token.colorBgContainer,
-      display: "flex",
-      minHeight: "100vh",
-      padding: screens.md ? `${token.sizeXXL}px 0px` : "0px",
-    },
-    text: {
-      color: token.colorTextSecondary,
-    },
-    title: {
-      fontSize: screens.md ? token.fontSizeHeading2 : token.fontSizeHeading3,
+    imageSection: {
+      flex: 1,
+      backgroundImage: `url(${picture})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      height: "100vh",
+      display: screens.md ? "block" : "none",
     },
   };
 
   return (
     <section style={styles.section}>
+      <div style={styles.imageSection}></div>
       <div style={styles.container}>
-        <div style={styles.header}>
-          <Title style={styles.title}>เข้าสู่ระบบ</Title>
-          <Text style={styles.text}>
-          กรุณากรอกรายละเอียดด้านล่างเพื่อเข้าสู่ระบบ
-          </Text>
-        </div>
-        <Form
-          name="normal_login"
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={onFinish}
-          layout="vertical"
-          requiredMark={false}
-        >
-          <Form.Item
-            name="email"
-            rules={[
-              {
-                type: "email",
-                required: true,
-                message: "กรุณาใส่อีเมลของคุณ!",
-              },
-            ]}
+        <div>
+          <div style={styles.header}>
+            <Title style={styles.title}>เข้าสู่ระบบ</Title>
+            <Text style={styles.text}>
+              กรุณากรอกรายละเอียดด้านล่างเพื่อเข้าสู่ระบบ
+            </Text>
+          </div>
+          <Form
+            name="normal_login"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            layout="vertical"
+            requiredMark={false}
           >
-            <Input prefix={<MailOutlined />} placeholder="ชื่อผู้ใช้หรืออีเมล" />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: "กรุณาใส่รหัสผ่านของคุณ!",
-              },
-            ]}
-          >
-            <Input.Password
-              prefix={<LockOutlined />}
-              type="password"
-              placeholder="รหัสผ่าน"
-            />
-          </Form.Item>
-
-          <Form.Item>
-            <Form.Item name="remember" valuePropName="checked" noStyle>
-              <Checkbox>จดจำฉันไว้</Checkbox>
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  type: "email",
+                  required: true,
+                  message: "กรุณาใส่อีเมลของคุณ!",
+                },
+              ]}
+            >
+              <Input
+                prefix={<MailOutlined />}
+                placeholder="ชื่อผู้ใช้หรืออีเมล"
+              />
             </Form.Item>
-          </Form.Item>
-
-          <Form.Item style={{ marginBottom: "0px" }}>
-            <Button block type="primary" htmlType="submit">
-              เข้าสู่ระบบ
-            </Button>
-            <div style={styles.footer}>
-              <Text style={styles.text}>ไม่มีบัญชีใช่ไหม?</Text>{" "}
-              <Link href="/register">ลงทะเบียน</Link>
-            </div>
-          </Form.Item>
-        </Form>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "กรุณาใส่รหัสผ่านของคุณ!",
+                },
+              ]}
+            >
+              <Input.Password
+                prefix={<LockOutlined />}
+                type="password"
+                placeholder="รหัสผ่าน"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>จดจำฉันไว้</Checkbox>
+              </Form.Item>
+            </Form.Item>
+            <Form.Item style={{ marginBottom: "0px" }}>
+              <Button block type="primary" htmlType="submit">
+                เข้าสู่ระบบ
+              </Button>
+              <div style={styles.footer}>
+                <Text style={styles.text}>ไม่มีบัญชีใช่ไหม?</Text>{" "}
+                <Link href="/register">ลงทะเบียน</Link>
+              </div>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
     </section>
-  );
+  );  
 }
