eed0cdeb5d9717c51920ccc74f212ed2f3f4cbf9
KamolwanSangnoo <pppor816@gmail.com>
2025-02-10 14:20:29 +0700

    wh

--- frontend/src/Navigation/Nav.jsx
+++ frontend/src/Navigation/Nav.jsx
@@ -32,7 +32,7 @@ const demoTheme = createTheme({
     MuiAppBar: {
       styleOverrides: {
         root: {
-          background: "rgb(4, 126, 14)",
+          background: "#38b000",
           // background: "linear-gradient(90deg, #38b000, #70e000)",
           color: "#FFFFFF",
         },
